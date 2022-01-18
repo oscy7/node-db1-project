4f60b0031415dd35e8fa89d42f4a61c484e4d2f6
Oscar Hernandez <oscarh425@outlook.com>
2022-01-17 21:15:43 -0800

    finished most of mvp

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,52 @@
+const Account = require('./accounts-model')
+const db = require('../../data/db-config')
+
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  const error = { status: 400 }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined ){
+    error.message = 'name and budget are required'
+    next(error)
+  } else if (typeof name !== 'string'){
+    error.message = 'name of account must be a string'
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100){
+    error.message = 'name of account must be between 3 and 100'
+    next(error)
+  } else if (typeof budget !== 'number' || isNaN(budget)){
+    error.message = 'budget of account must be a number'
+    next(error)
+  } else if (budget < 0 || budget > 1000000){
+    error.message = 'too large or too small'
+    next(error)
+  }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try { 
+    const uniqueName = await db('accounts').where('name', req.body.name.trim()).first()
+    if (uniqueName) {
+      next({status:400, message:'that name is taken'})
+    }else {
+      next()
+    }
+  } catch (err){
+    next(err)
+  }
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id)
+    if(!account){
+      next({
+        status: 404, message: 'account not found'
+      })
+    } else {
+      req.account = account
+      next()
+    }
+  } catch(err){
+    next(err)
+  }
 }
--- api/server.js
+++ api/server.js
@@ -1,7 +1,18 @@
+const { json } = require("express");
 const express = require("express");
+const accountsRouter = require('./accounts/accounts-router')
+
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter)
+
+server.use('*', (req,res) =>{
+    res.status(400).json({
+        message: 'account not found'
+    })
+})
 
 module.exports = server;
